a63391a7219e9be87dd265cf613370499f81e1a4
Shubhamkunwar10 <kshubham@c3ihub.ittk.ac.in>
2023-09-22 01:55:40 +0530

    cors

--- api/headless.js
+++ api/headless.js
@@ -1,18 +1,30 @@
 // api/proxy.js
 const fetch = require('node-fetch');
-const { send, json } = require('micro');
-const cors = require('micro-cors')();
 
-module.exports = cors(async (req, res) => {
+module.exports = async (req, res) => {
   const backendUrl = 'http://34.100.170.218:8002'; // Your HTTP backend URL
   const { method, headers, body } = req;
+
+  // Set CORS headers to allow requests from any origin
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Requested-With'
+  );
+
+  if (req.method === 'OPTIONS') {
+    // Handle preflight requests
+    res.status(200).end();
+    return;
+  }
 
   try {
     const response = await fetch(backendUrl, { method, headers, body });
     const data = await response.json();
-    send(res, response.status, data);
+    res.status(response.status).json(data);
   } catch (error) {
     console.error(error);
-    send(res, 500, { error: 'An error occurred while proxying the request.' });
+    res.status(500).json({ error: 'An error occurred while proxying the request.' });
   }
-});
+};
