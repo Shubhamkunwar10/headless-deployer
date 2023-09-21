12dc8fbd9b499b560f1c4555c7654811f6450ba4
Shubhamkunwar10 <kshubham@c3ihub.ittk.ac.in>
2023-09-22 02:22:08 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import fetch from 'node-fetch';
-import cors from 'cors'; // Import the cors middleware
+import cors from 'cors'; 
 
 const app = express();
 const port = process.env.PORT || 8005;
